f0c0a1685521abea117fa6323381391be4a84653
Kristian Nymann Jakobsen <kristian@nymann.dev>
2020-07-20 18:42:38 +0200

    :green_heart: Fixed CI build

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,17 @@
+"""Example Google style docstrings.
+
+"""
+import os
+import pathlib
+
 import pytest
 import sql_column_parser
-import pathlib
-import os
+
 
 @pytest.fixture()
 def parser():
+    """parser.
+    """
     test_directory = pathlib.Path(__file__).parent.absolute()
     path = os.path.join(test_directory, "test1.sql")
     return sql_column_parser.Parser(path)
-    
-
-    
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,7 +1,15 @@
-from typing import TYPE_CHECKING
+"""Example Google style docstrings.
+
+"""
 import sql_column_parser
 
+
 def test_parse(parser: sql_column_parser.Parser):
+    """test_parse.
+
+    Args:
+        parser (sql_column_parser.Parser): parser
+    """
     table = parser.parse()
     assert table.names.plural_name == "users"
     assert table.names.singular_name == "user"
